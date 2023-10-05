d95d7423c6f7d319d943eb91044b7dcef88db071
Marco Torres <marcoantonio.torres@icloud.com>
2023-10-05 10:03:02 -0400

    Implement some basic lifecycle hooks

--- my-app/src/app/products/product-detail/product-detail.component.ts
+++ my-app/src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit ,Output, EventEmitter, OnChanges, SimpleChange ,ViewEncapsulation, ChangeDetectionStrategy, SimpleChanges } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChange ,ViewEncapsulation, ChangeDetectionStrategy, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,29 +7,20 @@ import { Component, Input, OnInit ,Output, EventEmitter, OnChanges, SimpleChange
   encapsulation: ViewEncapsulation.None,  // Setting the ViewEncapsulation to none will allow the css style to leak out 
   changeDetection: ChangeDetectionStrategy.OnPush  // Will trigger the change detection mechanism only when the reference of the name input property changes
 })
-export class ProductDetailComponent implements OnInit, OnChanges{
-
-  
-  // The name saved in selected product won't show due to being undefined before string interpolation
-  constructor() {
-    console.log(`Name is ${this.name} in the constructor`);
-  }
-
-  // The name of selectedProduct will show here
-  ngOnInit(): void {
-    console.log(`Name is ${this.name} in the ngOnInit`);
-  }
-  
+export class ProductDetailComponent implements OnChanges{
 
   ngOnChanges(changes: SimpleChanges): void {
     const product = changes['name'];
-    const oldValue = product.previousValue;
-    const newValue = product.currentValue;
-    console.log(`Product change from ${oldValue} to ${newValue}`);
+    if (!product.isFirstChange()){  // if we selected a product then log out the old and new value
+      const oldValue = product.previousValue;  // old value currently contains the Microphone
+      const newValue = product.currentValue;  // new value becomes what we click on
+      console.log(`Product change from ${oldValue} to ${newValue}`);
+    }
   }
 
   // We use Input decorator to initialize an empty string
   @Input() name = '';
+  
 
   // We use Output decorator and it is initialized to a new EventEmitter 
   @Output() bought = new EventEmitter<string>();
@@ -38,12 +29,4 @@ export class ProductDetailComponent implements OnInit, OnChanges{
   buy() {
     this.bought.emit(this.name);
   }
-
-  // Create a getter to log out the selected product
-  get productName(): string {
-    console.log(`Get ${this.name}`);
-    return this.name;
-
-  }
-
 }
--- my-app/src/app/products/product-list/product-list.component.ts
+++ my-app/src/app/products/product-list/product-list.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import {  AfterViewInit, Component, ViewChild} from '@angular/core';
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    if (this.productDetail) {
+      console.log(this.productDetail.name);
+    }
+  }
 
   // Empty variable to store selected item
   selectedProduct = 'Microphone';
@@ -15,4 +22,9 @@ export class ProductListComponent {
   onBuy(name: string) {
     window.alert(`You just bought ${this.selectedProduct}`);
   }
+
+  // We query a child componenent from the parent component class
+  // When we query the productDetail property, we get an instance of the ProductDetailComponent class. 
+  // We can then access any memeber of its public API, such as the name property
+  @ViewChild(ProductDetailComponent) productDetail: ProductDetailComponent | undefined;
 }
