49e25560c31e2eaa7f65168bfaaa7bfa2b191577
Marco Torres <marcoantonio.torres@icloud.com>
2023-10-09 00:13:24 -0400

    Iterating through data using *ngFor and the template input variable, declaring a variable with a property in our *ngFor to access the index and display it in the template, and using trackBy to keep track of the nodes used in order to help the performance of the application

--- my-app/src/app/products/product-list/product-list.component.ts
+++ my-app/src/app/products/product-list/product-list.component.ts
@@ -8,13 +8,18 @@ import { ProductDetailComponent } from '../product-detail/product-detail.compone
 })
 export class ProductListComponent implements AfterViewInit {
 
+  // Create a array so we can use the ngFor directive to display the product list in a more scalable way
+  products = ['Webcam', 'Microphone', 'Wireless keyboard']
+
+  // Check if the productDetail property has been set
   ngAfterViewInit(): void {
-    if (this.productDetail) {
-      console.log(this.productDetail.name);
+    if (this.productDetail) {  // When we query the productDetail property, we get an instance of a ProductDetailComponent class
+      console.log(this.productDetail.name);  //  We can then access any member of its public API such as the name property
     }
   }
 
   // Empty variable to store selected item
+  // Investigate the reassining the variable property 
   selectedProduct = '';
 
 
@@ -27,4 +32,11 @@ export class ProductListComponent implements AfterViewInit {
   // When we query the productDetail property, we get an instance of the ProductDetailComponent class. 
   // We can then access any memeber of its public API, such as the name property
   @ViewChild(ProductDetailComponent) productDetail: ProductDetailComponent | undefined;
+
+  // This trackByProducts accepts two parameters with index of type number and a name of type string
+  trackByProducts(index: number, name:string): string {
+    // We return the actual product nam
+    return (name)
+    ;
+  }
 }
